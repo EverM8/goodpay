d5a4fb54fdee8ca13ab13e03334f6c1a4f8dbd48
EverM8 <evcarvalho93@gmail.com>
2022-10-03 17:14:25 -0300

    goodpay

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/**
+ * Trouxe o array do arquivo moduloDados.js para o index.js
+ */
+
+import usuarios from "./moduloDados.js";
+
+const btnEntrar=document.getElementById('btnEntrar')
+const alertaLogin=new bootstrap.Modal('#alertaLogin')
+
+btnEntrar.addEventListener('click', ()=>{
+    const inpunLogin=document.getElementById('login').value
+    const inputSenha=document.getElementById('senha').value
+
+    let usuarioValido;
+
+    for(let usuario of usuarios){
+        if(usuario.nomeUsuario==inpunLogin){
+            usuarioValido=usuario;
+            break
+        }
+    }
+
+    if(inputSenha==usuarioValido.senha){
+        window.location.pathname='/app.html'
+    }else{
+        alertaLogin.show()
+    }
+
+
+});
---
+++ js/moduloDados.js
@@ -0,0 +1,39 @@
+/**Dentro desse arrray existem usuários com esta estrutura:
+ * -id
+ * -nomeUsuario
+ * -senha
+ * -email 
+ *  */
+ 
+const usuarios=[
+    {
+        id: 1,
+        nomeUsuario:'nome1', 
+        senha: "123456",
+        email: 'emai-l@gmail.com'
+    },
+    {
+        id: 2,
+        nomeUsuario:'nome2',
+        senha: '123456',
+        email: 'email-2@gmail.com'
+    },
+    {
+        id: 3,
+        nomeUsuario:'nome3',
+        senha: '123456',
+        email: 'email-3@gmail.com'
+    },
+    {
+        id: 4,
+        nomeUsuario:'nome4',
+        senha: '123456',
+        email: 'email-4@gmail.com'
+    },
+]
+
+export default usuarios;
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
+
